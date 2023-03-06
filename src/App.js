b680a45a907c6746cabd5f43eadbb3543380f473
djo1975 <mladanilic1975@gmail.com>
2023-03-06 13:24:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+function App() {
+  return <main>
+    <Navbar />
+    <CartContainer/>
+  </main>;
+}
+export default App;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems'
+
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
+});
+
+export const { clearCart } = cartSlice.actions;
